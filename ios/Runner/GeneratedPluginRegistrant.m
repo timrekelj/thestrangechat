340a3fbf0e94301bce3d038295538ed9907637f7
timrekelj <tim.rekelj@gmail.com>
2020-03-10 22:29:46 +0100

    first commit

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<liquid_swipe/LiquidSwipePlugin.h>)
+#import <liquid_swipe/LiquidSwipePlugin.h>
+#else
+@import liquid_swipe;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [LiquidSwipePlugin registerWithRegistrar:[registry registrarForPlugin:@"LiquidSwipePlugin"]];
+}
+
+@end
